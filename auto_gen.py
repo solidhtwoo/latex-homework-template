ee0ab28652d5d437fc314222a82af0ecac88296a
solidhtwoo <174594@stu.hebut.edu.cn>
2020-02-27 20:49:26 +0800

    Update auto_gen.py

--- auto_gen.py
+++ auto_gen.py
@@ -1,6 +1,30 @@
 # -*- coding: UTF-8 -*-
 import sys, getopt, os, datetime,requests
 from os.path import expanduser
+
+def handle_dates(arg, due_dater):
+    cur_time = datetime.datetime.today()
+    if (arg[0] == '!') :
+        due_dater = arg[1:] + due_dater
+    elif ('+' in arg) or ('-' in arg) :
+        try:
+            ch = '+' if '+' in arg else '-'
+            symb = 1 if ch == '+' else -1
+            lstr,rstr = map(str.strip, arg.strip().split(ch))
+            print(lstr)
+            base = get_date(lstr)
+            delta = datetime.timedelta(days=(int(rstr) * symb))
+            base += delta
+            due_dater = to_time_str(base)
+        except EOFError:
+            print("!")
+            sys.exit(2)
+    elif (arg in ("t","to","today","tomorrow","yesterday","y")) : 
+        due_dater = to_time_str(get_date(arg))
+    elif (len(arg) == 8 and str.isdigit(arg)) :
+        due_dater = str(int(arg[0 : 4])) +" \\ 年 \\ " + str(int(arg[4 : 6])) + " \\ 月 \\ " + str(int(arg[6 : 8])) + " \\ 日 " + due_dater
+    print(due_dater)
+    return due_dater
 def init_conf(path):
     author_idl = "\\newcommand{\\hmwkAuthorID}{"
     author_classl = "\\newcommand{\\hmwkAuthorClass}{"
@@ -43,12 +67,17 @@ def main(argv):
     instructorl = "\\newcommand{\\hmwkClassInstructor}{}"
     chk_len_ins = len(instructorl)
     chk_len_ct = len(class_timel)
+    finish_time = "\\date{"
     hp = '''
 文档生成器,用于生成填好个人信息的文档
 参数:
 -h, --help 显示帮助 
 -t, --title= 作业标题
 -s, --sub= 科目
+-f, --finish-time 署名下的日期,一般是完成日期, 默认生成为今天, 
+           可以使用today(也可写成t), tomorrow(也可写成to), yesterday(也可写成y), yyyyMMdd,
+           如果想表示相对时间, 如today+2表示后天,today-1, 表示昨天(不支持乘除操作).
+           其他情况先输入!再输入自定义的时间如!1145141919, 会在文档中原样显示 
 -d, --due= 截止时间, 可以使用today(也可写成t), tomorrow(也可写成to), yesterday(也可写成y), yyyyMMdd,
            如果想表示相对时间, 如today+2表示后天,today-1, 表示昨天(不支持乘除操作).
            其他情况先输入!再输入自定义的时间如!1145141919, 会在文档中原样显示
@@ -60,7 +89,7 @@ def main(argv):
 '''
     test_run_flg = True
     try:
-        opts, args = getopt.getopt(argv,"ht:s:d:r:i:c:",["title=","sub=","due=","help","reset","no-test-run","instructor","class-time"])
+        opts, args = getopt.getopt(argv,"ht:s:d:r:i:c:f:",["title=","sub=","due=","help","reset","no-test-run","instructor","class-time","finish-time"])
     except getopt.GetoptError:
         print(hp)
         sys.exit(2)
@@ -84,28 +113,10 @@ def main(argv):
         elif opt in ("-s", "--sub"):
             class_namer = arg + class_namer
             print(class_namer)
+        elif opt in ("-f","--finish-time") :
+            finish_time = finish_time + handle_dates(arg, "}")
         elif opt in ("-d","--due"):
-            cur_time = datetime.datetime.today()
-            if (arg[0] == '!') :
-                due_dater = arg[1:] + due_dater
-            elif ('+' in arg) or ('-' in arg) :
-                try:
-                    ch = '+' if '+' in arg else '-'
-                    symb = 1 if ch == '+' else -1
-                    lstr,rstr = map(str.strip, arg.strip().split(ch))
-                    print(lstr)
-                    base = get_date(lstr)
-                    delta = datetime.timedelta(days=(int(rstr) * symb))
-                    base += delta
-                    due_dater = to_time_str(base)
-                except EOFError:
-                    print("!" + hp)
-                    sys.exit(2)
-            elif (arg in ("t","to","today","tomorrow","yesterday","y")) : 
-                due_dater = to_time_str(get_date(arg))
-            elif (len(arg) == 8 and str.isdigit(arg)) :
-                due_dater = str(int(arg[0 : 4])) +" \\ 年 \\ " + str(int(arg[4 : 6])) + " \\ 月 \\ " + str(int(arg[6 : 8])) + " \\ 日 " + due_dater
-            print(due_dater)
+            due_dater = handle_dates(arg, due_dater)
         elif opt in ("-r", "--reset"):
             init_conf(file_path)
             sys.exit()
@@ -135,6 +146,8 @@ def main(argv):
         tmp = requests.get(hm_url)
         tmp.encoding = 'utf-8'
         hm.write(tmp.text)
+    if (finish_time == "\\date{") :
+        finish_time = "\\date{\\today}"
     with open(file_path, 'r', encoding='utf-8') as fi:
         with open("info.tex", 'w', encoding='utf-8') as fo:
             for ln in fi.readlines():
@@ -144,6 +157,7 @@ def main(argv):
             fo.write(due_datel + due_dater + '\n')
             fo.write(instructorl + "\n")
             fo.write(class_timel + '\n')
+            fo.write(finish_time + "\n")
     if(test_run_flg) :
         print(os.system("latexmk -xelatex -latexoption=-file-line-error -latexoption=-interaction=nonstopmode -latexoption=-shell-escape homework.tex"))
 if __name__ == "__main__":
